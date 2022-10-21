afc07df42147db40fc7743777696dff1495ea2eb
Frank Stekelenburg <frankstekelenburg@ziggo.nl>
2022-10-21 20:44:11 +0200

    Added custom dataset  + fixed error with deleting images
    go to DeepLearning folder for using openCV other wise you will get errors

---
+++ convolutionalNeural.py
@@ -0,0 +1,161 @@
+# importing libraries
+import cv2
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+import keras.layers as layers
+from keras.models import Model
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten
+
+#loading dataset
+
+#clearing dataset from corrupted images
+num_passed = 0
+for images in ("frank", "caroline"):
+    folder_path = os.path.join('C:\FaceDetection\DeepLearning\images', images)
+    for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        try:
+            fobj = open(fpath, "rb")
+            is_jpg = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+        finally:
+            fobj.close()
+
+        if not is_jpg:
+            num_passed += 1
+            #Delete corrupted images
+            os.remove(fpath)
+
+print("Deleted items %d" % num_passed)
+
+image_size = (180, 180)
+batch_size = 32
+
+labels = ("Frank", "Caroline")
+
+for images in labels:
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "C:\FaceDetection\DeepLearning\images",
+    labels="inferred",
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+    )
+
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "C:\FaceDetection\DeepLearning\images",
+    validation_split=0.2,
+    subset="validation",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+    )
+
+plt.figure(figsize=(10,10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3,3, i + 1)
+        plt.imshow(images[1].numpy().astype("uint8"))
+        plt.title(int(labels[i]))
+        plt.axis("off")
+
+# (train_X, train_y), (val_X, val_y) = mnist.load_data()
+
+#normalizing the dataset
+# train_X, val_X = train_X/255, val_X/255
+
+# visualizing 9 rndom digits from the dataset
+# for i in range(331,340):
+#     plt.subplot(i)
+#     a = np.random.randint(0, train_X.shape[0], 1)
+#     plt.imshow(train_X[a[0]], cmap = plt.get_cmap('binary'))
+
+# plt.tight_layout()
+# plt.show()
+
+def create_model(input_shape = (28,28,1)):
+    model = keras.Sequential([
+    layers.Conv2D(filters = 32, kernel_size = 3, activation = 'relu', padding = 'same', input_shape = input_shape),
+    layers.MaxPool2D(pool_size = 2),
+    
+    layers.Conv2D(filters = 64, kernel_size = 3, activation = 'relu', padding = 'same'),
+    layers.MaxPool2D(pool_size = 2),
+    
+    layers.Conv2D(filters = 128, kernel_size = 3, activation = 'relu', padding = 'same'),
+    layers.MaxPool2D(pool_size = 2),
+    
+    layers.Flatten(),
+    layers.Dense(units = 54, activation = 'relu'),
+    layers.Dense(units = 10, activation = 'softmax')])
+    
+    return model
+
+# def compile_model(model, optimizer='adam', loss='categorical_crossentropy'):
+#     model.compile(optimizer=optimizer, loss=loss, metrics=["accuracy"])
+
+# def fitting_model(model, x, y, epoch):
+#     model.fit(x,y, shuffle = True, epochs = epoch)
+
+# #reshaping the independant variables
+# train_X = train_X.reshape(train_X.shape[0], 28, 28, 1)
+# val_X = val_X .reshape(val_X.shape[0], 28, 28, 1)
+
+# #encoding the dependant variable
+# train_y = np.eye(10)[train_y]
+# val_y = np.eye(10)[val_y]
+
+# #creating model
+# model = create_model((28,28,1))
+# #optimizing model
+# compile_model(model, 'adam', 'categorical_crossentropy')
+
+# #training model
+# history = model.fit(train_X, train_y, validation_data = (val_X, val_y), batch_size = 150, epochs = 80)
+# # model.save("cnn_digitclass.model") #model will be save in root folder to be later called out for prediction
+
+# #model performance visualization
+# f = plt.figure(figsize=(20,8))
+
+# #accuracy
+# plt1 = f.add_subplot(121)
+# plt1.plot(history.history['accuracy'], label = str('Training accuracy'))
+# plt1.plot(history.history['val_accuracy'], label = str('Validation accuracy'))
+# plt.legend()
+# plt.title('accuracy')
+
+# #loss
+# plt2 = f.add_subplot(122)
+# plt2.plot(history.history['loss'], label = str('Training loss'))
+# plt2.plot(history.history['val_loss'], label = str('Validation loss'))
+# plt.legend()
+# plt.title('loss')
+
+# plt.show()
+while True:
+    img = cv2.imread("sample_img.jpg") #loading input image
+    img = cv2.resize(img, (28, 28), interpolation = cv2.INTER_AREA) #resizing to input shape
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #chaging to grayscale format
+    img = cv2.bitwise_not(img) #the color scale was inverted, correcting inverted color scale
+    img = cv2.Canny(img, 50, 50) # removing noise
+    cv2.imshow('image',img)
+    keyboard_input = cv2.waitKey(1)
+    # 27 is the ASCII for the esc key on your keyboard.
+    if keyboard_input == 27:
+        break
+
+
+predict_data = np.array([img])/255 #changing image data to array
+predict_data = predict_data.reshape(1,28, 28, 1) #reshaping to input shape
+
+#predicting the input 
+from keras import models
+model = models.load_model('cnn_digitclass.model') #loading pre-savedd model
+prediction = model.predict(predict_data) #gives array
+predicted_number = np.argmax(prediction) #extracts predicted number
+print(predicted_number)
--- testModel.py
+++ testModel.py
@@ -1,8 +1,11 @@
+import tensorflow as tf
+from tensorflow import keras
 import cv2
 import numpy as np
 from keras.models import load_model
 import numpy as np
 
+facedetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Load the model
 model = load_model('keras_model.h5')
@@ -14,28 +17,48 @@ camera.set(4, 480)
 
 # Grab the labels from the labels.txt file. This will be used later.
 labels = open('labels.txt', 'r').readlines()
+font = cv2.FONT_HERSHEY_COMPLEX
+
+def get_className(classNo):
+    if classNo == 0:
+        return "Frank"
+    elif classNo == 1:
+        return "Caroline"
 
 while True:
     # Grab the webcameras image.
     ret, frame = camera.read()
-    crop_image = frame
-    # Resize the raw image into (224-height,224-width) pixels.
-    image = cv2.resize(crop_image, (224, 224), interpolation=cv2.INTER_AREA)
-    image = image.reshape(1, 224, 224, 3)
-    # Show the image in a window
+    faces = faces = facedetect.detectMultiScale(frame, 1.3, 5)
+    for x, y, w, h in faces:
+        crop_image = frame
+        # Resize the raw image into (224-height,224-width) pixels.
+        image = cv2.resize(crop_image, (224, 224), interpolation=cv2.INTER_AREA)
+        image = image.reshape(1, 224, 224, 3)
+        # Make the image a numpy array and reshape it to the models input shape.
+        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+        # Normalize the image array
+        image = (image / 127.5) - 1
+        # Have the model predict what the current image is. Model.predict
+        # returns an array of percentages. Example:[0.2,0.8] meaning its 20% sure
+        # it is the first label and 80% sure its the second label.
+        probabilities = model.predict(image)
+        # Print what the highest value probabilitie label
+        classIndex = np.argmax(probabilities)
+        print(classIndex)
+        probabilityValue = np.amax(probabilities)
+        if classIndex == 0:
+            print("test")
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x, y-40), (x+w, y), (0, 255, 0), -2)
+            cv2.putText(frame, str(get_className(classIndex)), (x, y-10), font, 0.75, (255, 255, 255), 1, cv2.LINE_AA)
+        elif classIndex == 1:
+            cv2.rectangle(frame, (x, y), (x+w,y+h),(0,255,0),2)
+            cv2.rectangle(frame, (x, y-40), (x+w, y), (0, 255,0),-2)
+            cv2.putText(frame, str(get_className(classIndex)), (x, y-10), font, 0.75, (255, 255,255),1, cv2.LINE_AA)
+        cv2.putText(frame, str(round(probabilityValue*100, 2))+"%" , (180, 75), font, 0.75, (255, 0,0),2, cv2.LINE_AA)
+        # Listen to the keyboard for presses.
+        # Show the image in a window
     cv2.imshow('Frame', frame)
-    # Make the image a numpy array and reshape it to the models input shape.
-    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-    # Normalize the image array
-    image = (image / 127.5) - 1
-    # Have the model predict what the current image is. Model.predict
-    # returns an array of percentages. Example:[0.2,0.8] meaning its 20% sure
-    # it is the first label and 80% sure its the second label.
-    probabilities = model.predict(image)
-    # Print what the highest value probabilitie label
-    classIndex = labels[np.argmax(probabilities)]
-    print(classIndex)
-    # Listen to the keyboard for presses.
     keyboard_input = cv2.waitKey(1)
     # 27 is the ASCII for the esc key on your keyboard.
     if keyboard_input == 27:
